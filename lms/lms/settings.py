0bab15f1f28e77fa9b43325b7659a9e0a40f94eb
parth-harpal <161468602+parth-harpal@users.noreply.github.com>
2026-04-30 00:51:12 +0530

    Settings.py changed

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -174,7 +174,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'parthharpal37@gmail.com'
-EMAIL_HOST_PASSWORD = 'enqqsxnwslcpnknn'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL='Celery <parthharpal37@gmail.com>'
 
 
